b3bbf615f71c0a9652701d10e6ed6b70f16cd0db
wulfiez <vostrikovnikita34@gmail.com>
2022-04-07 22:50:25 +0700

    Add project files.

---
+++ Blockchain/Blockchain.h
@@ -0,0 +1,23 @@
+#ifndef Blockchain_h
+#define Blockchain_h
+
+#include <vector>
+
+class Blockchain
+{
+private:
+    Block createGenesisBlock();
+    std::vector<Block> chain;
+
+public:
+    Blockchain();
+
+    std::vector<Block> getChain();
+    Block* getLatestBlock();
+    bool isChainValid();
+    void addBlock(TransactionData data);
+    void printChain();
+};
+
+#endif
+
---
+++ Blockchain/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <iostream>
+
+#include "TransactionData.h"
+#include "Block.h"
+#include "Blockchain.h"
+
+using namespace std;
+
+int main()
+{
+    Blockchain awesomeCoin;
+    
+
+    return 0;
+}
